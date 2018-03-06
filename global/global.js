ec5573a889bd16f8a10223690cdd242547cecbc3
Yuan Tan <2578443177@qq.com>
2018-03-06 20:56:35 +0800

    LI
    新的云音乐

---
+++ global/global.js
@@ -0,0 +1,103 @@
+//js封装组件
+//ui组件
+var ui={
+	showAlert:function(text,callback){
+		var alert=$('body').putDiv('alert normal',text);
+		setTimeout(function(){
+			if(callback) callback();
+			alert.remove();
+		},2000)
+	},
+	showConfrim:function(text,callback){
+		var confrim=$('body').putDiv('confrim',text);
+		var buttonGroup=confrim.putDiv('buttonGroup');
+		buttonGroup.put('button',['yes','no'],['确认','取消']);
+		$('.yes,.no').click(function(){
+			confrim.addClass('active');
+			if($(this).hasClass('yes')) callback();
+			confrim.hide();
+		});
+	},
+};
+//系统组件
+var sys={
+	checkReg:function(obj,mode){
+		for (let i in obj) {
+			if(obj[i].length==0) return 0;
+			if(obj[i].length<6||obj[i].length>15) return 1;
+		}
+		var reg;
+		mode=mode||'';
+		switch(mode){
+			case 'remix':reg=/[^A-Za-z0-9_\-\u4e00-\u9fa5]+/g;break;//包括汉字
+			default:reg=/\W+/g;break;
+		}
+		return reg.test(obj.username);
+	},
+};
+//网络组件
+var net={
+	sign:function(obj,callback){
+		obj.type='sign';
+		this.post(obj,'注册成功',callback);
+	},
+	login:function(obj,callback){
+		obj.type='login';
+		this.post(obj,'登录成功',callback);
+	},
+	post:function(obj,prompt,callback){
+		var url="http://localhost/php/ls.php/";
+		$.post(url,obj,function(data){
+			console.log(data);
+			if(data==prompt) callback=function(){
+				console.log('ch!');
+			};
+			ui.showAlert(data,callback);
+		},'text');
+	},
+}
+$.fn.extend({
+	put: function(type, className, innerHTML) {	
+		className = className||'';
+		innerHTML = innerHTML||'';
+		//批量put
+		if(Array.isArray(className)&&Array.isArray(innerHTML)){
+			var list=[];
+			for(var i=0;i<className.length;i++){
+				var node = $("<" + type + "></" + type + ">");
+				node.addClass(className[i]);
+				node.text(innerHTML[i]);
+				list.push(node);
+			}
+			$(this).append(list);
+		}
+		else{
+			var node = $("<" + type + "></" + type + ">");
+			node.addClass(className);
+			node.text(innerHTML);
+			$(this).append(node);
+			return node;
+		}
+	},
+	putDiv: function(className, innerHTML) {
+		className = className||'';
+		innerHTML = innerHTML||'';	
+		if(Array.isArray(className)&&Array.isArray(innerHTML)){
+			var list=[];
+			for(var i=0;i<className.length;i++){
+				var div = $("<div></div>");
+				div.addClass(className[i]);
+				div.text(innerHTML[i]);
+				list.push(div);
+			}
+			$(this).append(list);
+		}
+		else{
+			var div = $("<div></div>");
+			div.addClass(className);
+			div.text(innerHTML);
+			$(this).append(div);
+			return div;
+		}
+	},
+})
---
+++ page/ls/ls.js
@@ -0,0 +1,45 @@
+(function(){
+	'use strict'	
+	$(document).ready(function() {
+		$('.tab_item').click(function() {
+			$(this).addClass('active');
+			$(this).siblings().removeClass('active');
+			var id = $(this).index(); //当前操作的元素索引值 
+			var number1 = 80 + id * 120;
+			var number2 = -id * 360;
+			$('.slider').animate({
+				marginLeft: number1
+			});
+			$('.main').animate({
+				marginLeft: number2
+			});
+		});
+		
+		$('.sumbit').click(function(){
+			//设置禁用防止多次提交
+			var that=$(this);
+			var text=that.text();
+			that.attr('disabled','true').text('提交中...');
+			var username=that.siblings('.username').val();
+			var password=that.siblings('.password').val();
+			var userInfo={
+				username:username,
+				password:password
+			}
+			function callback(){
+				that.removeAttr('disabled').text(text);
+			}
+			//先检查正则相关问题，根据返回值进行相应处理
+			var result=sys.checkReg(userInfo);
+			switch(result){
+				case 0:ui.showAlert('用户名及密码不能为空',callback);break;
+				case 1:ui.showAlert('用户名及密码均需6-15位以内',callback);break;
+				case true:ui.showAlert('用户名需为字母数字组合',callback);break;
+				case false:
+					if(text=='注册') net.sign(userInfo,callback);
+					else net.login(userInfo,callback);
+				break;
+			}
+		});
+	})
+}())
